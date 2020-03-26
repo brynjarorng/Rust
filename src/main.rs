f78686709a098568ea26f287be0bfe52ab0d847e
G0rocks <44577486+G0rocks@users.noreply.github.com>
2020-03-26 22:44:37 +0100

    Source code

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+/*
+This is Huldars attempt at creating a game of snake using the programming language Rust
+Huldar is a total noob but will try his best ^_^
+*******************************************************************************************
+Creation date: 26.3.2020 at 19:40 (UTC+1)
+Author: Huldar
+License: Anybody can do pretty much whatever they want with this code.
+*******************************************************************************************
+
+How it will work.
+You run the program and the _snake_ will start moving.
+The snake can go through a wall on the left side and emerge from the right side
+Every now and then an "apple" will appear on the screen which if the snake "eats" it, then the snake will grow by 1 "step"
+The user can use the arrow keys to move the snake around.
+As the snake "eats" more "apples" he not only grows but also gains speed.
+The game is over when the snake touches itself or if there is no more space on the screen for the snake.
+*/
+
+//Eat apple function
+
+//Step function
+
+fn main() {
+    //Variable declaration
+        //Speed
+        //Length
+        //Direction?
+        //Boolean game over?
+
+
+    //Cool screen with "GAME STARTING" + "3" + "2" + "1" "BEGIN"
+
+    //Loop - While Game == notover && make sure the runtime of the loop is controlled by the speed
+
+        //Change direction?
+
+        //Step forward
+    
+            //Include if "crash" into wall
+    
+            //Include if apple was eaten
+    
+            //Include if crash into self
+
+        //Check if game is over to break loop
+
+    //When game is over make a cool ASCII "artwork" with a GAME OVER or a YOU WON
+    
+    println!("If this prints out, that means the program is working :)");
+}
